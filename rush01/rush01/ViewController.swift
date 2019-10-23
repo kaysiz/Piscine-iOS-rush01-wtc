6875f39c4138cd1010ebda6f584c479d7888bbc4
Ruan DE BEER <rde-beer@c4r25s2.wethinkcode.co.za>
2019-10-23 14:43:21 +0200

    pods installed

---
+++ rush01/rush01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rush01
+//
+//  Created by Ruan DE BEER on 2019/10/23.
+//  Copyright © 2019 Ruan DE BEER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
